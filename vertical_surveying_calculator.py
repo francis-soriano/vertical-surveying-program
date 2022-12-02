6a8422d96af9889b670474a10ec5f9bd38650839
SamuelPethick <sapethic@flemingcollege.ca>
2022-12-02 09:29:21 -0500

    Got the Calculator working - sapethic

--- vertical_surveying_calculator.py
+++ vertical_surveying_calculator.py
@@ -36,7 +36,7 @@ or other formats for GIS use)
 
 --------""")
 
-# --------------- Part I: Metadata ---------------
+# # --------------- Part I: Metadata ---------------
 
 print("""I. Metadata
 
@@ -44,10 +44,10 @@ A. Date (date information will be collected using
 yyyy-mm-dd format)
 """)
 
-# fsoriano: need to run the chunk of code below inside a try statement
-# that would show "data validation" in our program. honestly certain
-# chunks of code should be run in try statements. i'm a little suspicious
-# if Karen will try and break the code with erroneous entries lol
+# # fsoriano: need to run the chunk of code below inside a try statement
+# # that would show "data validation" in our program. honestly certain
+# # chunks of code should be run in try statements. i'm a little suspicious
+# # if Karen will try and break the code with erroneous entries lol
 
 while True:
     try:
@@ -81,47 +81,47 @@ B: Crew Members (crew information will be collected
 using first name initial, last name format)
 """)
 
-# fsoriano: need to loop into a tuple. 
-# first tuple is last name, second tuple is first name.
-# tbh idk, can be a list too.
+# # fsoriano: need to loop into a tuple. 
+# # first tuple is last name, second tuple is first name.
+# # tbh idk, can be a list too.
 
-# -------------- Design Document Specifications ---------------
-# | Please enter the first name initial of the survey         |
-# | party chief here: XXXXXXXXXXXX                            |
-# | Please enter the last name of the survey party chief      |
-# | here: XXXXXXXXXXXX                                        |
-# | Did you want to add more crew members? (Y / N) X          |
-# -------------------------- E N D ----------------------------
-
-while True:
-    try:
-        crew_members_party_chief = str(input("Please enter the first name initial of the survey party chief here:\n"))
-    except ValueError:
-        print("You have not entered a letter for the first name initial. Please try again.")
-    else:
-        break
+# # -------------- Design Document Specifications ---------------
+# # | Please enter the first name initial of the survey         |
+# # | party chief here: XXXXXXXXXXXX                            |
+# # | Please enter the last name of the survey party chief      |
+# # | here: XXXXXXXXXXXX                                        |
+# # | Did you want to add more crew members? (Y / N) X          |
+# # -------------------------- E N D ----------------------------
 
 while True:
-    try:
-        crew_members_more = str(input("Did you want to add more crew members? (Y / N)\n"))
-    except ValueError:
-        print("You need to answer 'Y' for YES and 'N' for NO. Please try again.")
-    else:
-        break
+     try:
+         crew_members_party_chief = str(input("Please enter the first name initial of the survey party chief here:\n"))
+     except ValueError:
+         print("You have not entered a letter for the first name initial. Please try again.")
+     else:
+         break
 
-print("C: Equipment")
+while True:
+     try:
+         crew_members_more = str(input("Did you want to add more crew members? (Y / N)\n"))
+     except ValueError:
+         print("You need to answer 'Y' for YES and 'N' for NO. Please try again.")
+     else:
+         break
 
-# fsoriano: part C also needs to get looped into a tuple/list.
-# maybe we can do up a function so that we don't crowd the code? 
+# print("C: Equipment")
 
-# -------------- Design Document Specifications ---------------
-# | Please enter the equipment item here: XXXXXXXXXXXX        |
-# | Did you want to enter another equipment item? (Y / N) X   |
-# -------------------------- E N D ----------------------------
+# # fsoriano: part C also needs to get looped into a tuple/list.
+# # maybe we can do up a function so that we don't crowd the code? 
 
-# fsoriano: part D *also* needs to get looped into a tuple/list.
-# plz consult design document thanks (i got too lazy to copy and
-# paste the design document lol)
+# # -------------- Design Document Specifications ---------------
+# # | Please enter the equipment item here: XXXXXXXXXXXX        |
+# # | Did you want to enter another equipment item? (Y / N) X   |
+# # -------------------------- E N D ----------------------------
+
+# # fsoriano: part D *also* needs to get looped into a tuple/list.
+# # plz consult design document thanks (i got too lazy to copy and
+# # paste the design document lol)
 
 print("""D: Weather (weather is a choice option, 1 to 4 and the 
 temperature in Celsius)
@@ -151,10 +151,10 @@ Ylist = [] # Creates a list for longitude
 print("Please Enter the Following: ")
 
 while True:
-    Foresight = float(input("The Foresight to the next station: ")) 
+    Foresight = float(input("The Foresight to the next station (If first station leave as 0): ")) 
     ForesightList.append(Foresight) # adds the foresight to the list
     
-    Backsight = float(input("The Backsight to the previous station: ")) 
+    Backsight = float(input("The Backsight to the previous station (If last station leave as 0): ")) 
     BacksightList.append(Backsight)	# adds the backsight to the list
 
     x = float(input("The Latitude of the station: ")) 
@@ -167,9 +167,22 @@ while True:
     print()
     end = input("Did you have another station (Y/N)? : ")
     print()
-    if  end.upper() == 'N' :
+    if  end.upper() == 'N' : #Should make it so that lower case n inputs also work
         break
-
+    else:
+        try: 
+            StationClass = str(input("The Station is a Benchmark rather than a Turning Point? (Y/N): ")) #Turning points in test values don't have coordinate points, in such cases they will be hidden for intermittent calculations.
+            if StationClass.upper == "Y": # Makes lower case y inputs work
+                x = float(input("The Latitude of the station: ")) 
+                Xlist.append(x)	# adds the latitude to the list
+                y = float(input("The Longitude of the station: ")) 
+                Ylist.append(y)	# adds the longitude to the list
+    
+            else: 
+                 print("Coordinates not required for a Turning Point. ")
+        except ValueError:
+             print("Please Enter an approrpriate Value, Y or N for the Station Class, and a number for Latitude and Longitude. ") # Should refrain from causing a program crash when inputs are mismatched.
+                    
 # End of Inputs
 
 # Calculations
@@ -177,11 +190,13 @@ while True:
 PointElevationList = [] # Creates an empty list to append calculated elevation into
 InstrumentHeightList = [] # Creates an empty list to append calculated height of instrument into
 
-for index in range [0,len(ForesightList)]: # Creates an index within the range of the above inputs
+for index in range(0,(len(ForesightList))): # Creates an index within the range of the above inputs
     Foresight = ForesightList[index] # retrieves the Foresight from the list for calculations 
     Backsight = BacksightList[index] # retrives the Backsight from the list for calculations
     Elevation, InstrumentHeight = ElevationCalculator(Backsight, Foresight, StartingElevation) # calls the elevation calculations function to return elevation and Instrument Height
     PointElevationList.append(Elevation) # appends calculated elevation into the Point elevation list
     InstrumentHeightList.append(InstrumentHeight) # appends calculated Instrument Height into the list
-
+    print("test")
+    print(PointElevationList)
+    print(InstrumentHeightList)
 # PART III: Output Options
