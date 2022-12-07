0804a5ee659e018b9bd8af832749d9f3f665a24b
SamuelPethick <sapethic@flemingcollege.ca>
2022-12-07 10:56:20 -0500

    Got the Station Class working properly in the while loop, should now be able to omit coordinates in calculations but still input turning points for itermittent calculations - sapethic

--- vertical_surveying_calculator.py
+++ vertical_surveying_calculator.py
@@ -208,38 +208,36 @@ Xlist = [] # Creates a list for latitude
 Ylist = [] # Creates a list for longitude
 print("Please Enter the Following: ")
 
-while True: # While Loop goes until the user specifies a break, allowing for traverses of all sizes.
-    Foresight = float(input("The Foresight to the next station (If first station leave as 0): ")) 
-    ForesightList.append(Foresight) # adds the foresight to the list
+try:
+    while True: # While Loop goes until the user specifies a break, allowing for traverses of all sizes.
+        Foresight = float(input("The Foresight to the next station (If first station leave as 0): ")) 
+        ForesightList.append(Foresight) # adds the foresight to the list
     
-    Backsight = float(input("The Backsight to the previous station (If last station leave as 0): ")) 
-    BacksightList.append(Backsight)	# adds the backsight to the list
+        Backsight = float(input("The Backsight to the previous station (If last station leave as 0): ")) 
+        BacksightList.append(Backsight)	# adds the backsight to the list
 
-    x = float(input("The Northing of the station: ")) 
-    Xlist.append(x)	# adds the latitude to the list
+        StationClass = str(input("Do you have Coordinates for this Station? (Y for Yes, Any Other Input Will be Assumed to be No.) : ")) # Turning points in test values don't necessarily have coordinate points, in such cases they will be hidden for intermittent calculations.
+        if StationClass == "Y" or StationClass == "y": # Makes lower case y inputs work
 
-    y = float(input("The Easting of the station: ")) 
-    Ylist.append(y)	# adds the longitude to the list
+            x = float(input("The Northing of the station: ")) 
+            Xlist.append(x)	# adds the latitude to the list
 
-    # Determine whether user needs to input another station 
-    print()
-    end = input("Did you have another station (Y/N)? : ")
-    print()
-    if  end.upper() == 'N' : # Should make it so that lower case n inputs also work
-        break
-    else:
-        try: 
-            StationClass = str(input("The Station is a Benchmark rather than a Turning Point? (Y/N): ")) #Turning points in test values don't necessarily have coordinate points, in such cases they will be hidden for intermittent calculations.
-            if StationClass.upper == "Y": # Makes lower case y inputs work
-                x = float(input("The Northing of the station: ")) 
-                Xlist.append(x)	# adds the latitude to the list
-                y = float(input("The Easting of the station: ")) 
-                Ylist.append(y)	# adds the longitude to the list
-    
-            else: 
-                print("Coordinates not required for a Turning Point. ")
-        except ValueError:
-            print("Please Enter an approrpriate Value, Y or N for the Station Class, and a number for Latitude and Longitude. ") # Should refrain from causing a program crash when inputs are mismatched from their data type.
+            y = float(input("The Easting of the station: ")) 
+            Ylist.append(y)	# adds the longitude to the list
+
+        # Determine whether user needs to input another station       
+        print()
+        end = input("Did you have another station (Y/N)? : ")
+        print()
+        if  end.upper() == 'N' : # Should make it so that lower case n inputs also work
+            break
+except ValueError:
+    print("Value Error: Please Enter Appropriate Inputs to our Specifications.")
+except TypeError:
+    print("Type Error: Please Enter Appropriate Inputs to our Specifications.")
+except Exception as message:
+    message = "A General Error has occured, Please try rerunning the program."
+    print("Error:", message)
                     
 # End of Inputs
 
